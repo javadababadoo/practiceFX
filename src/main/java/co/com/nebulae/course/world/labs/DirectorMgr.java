c18d01c9d1d6f24c52767528736db5cad6d85ac4
Sebastian <sebastianz1996@hotmail.com>
2016-11-16 06:27:42 -0500

    Freeball refactorizacion

--- src/main/java/co/com/nebulae/course/world/labs/DirectorMgr.java
+++ src/main/java/co/com/nebulae/course/world/labs/DirectorMgr.java
@@ -6,6 +6,7 @@
 package co.com.nebulae.course.world.labs;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,7 +14,14 @@ import java.util.ArrayList;
  */
 public class DirectorMgr {
 
+    private List<WorldShape> dynamicObjects = new ArrayList<>();
+
+    private List<WorldShape> staticObjects = new ArrayList<>();
+
+    private List<WorldShape> partialDynamicObjects = new ArrayList<>();
+
     //<editor-fold defaultstate="collapsed" desc="SINGLETON">
+
     private DirectorMgr() {
     }
 
@@ -21,11 +29,62 @@ public class DirectorMgr {
         return ElementMgrHolder.INSTANCE;
     }
 
+    /**
+     * @return the dynamicObjects
+     */
+    public List<WorldShape> getDynamicObjects() {
+        return dynamicObjects;
+    }
+
+    /**
+     * @param dynamicObjects the dynamicObjects to set
+     */
+    public void setDynamicObjects(List<WorldShape> dynamicObjects) {
+        this.dynamicObjects = dynamicObjects;
+    }
+
+    /**
+     * @return the staticObjects
+     */
+    public List<WorldShape> getStaticObjects() {
+        return staticObjects;
+    }
+
+    /**
+     * @param staticObjects the staticObjects to set
+     */
+    public void setStaticObjects(List<WorldShape> staticObjects) {
+        this.staticObjects = staticObjects;
+    }
+
+    /**
+     * @return the partialDynamicObjects
+     */
+    public List<WorldShape> getPartialDynamicObjects() {
+        return partialDynamicObjects;
+    }
+
+    /**
+     * @param partialDynamicObjects the partialDynamicObjects to set
+     */
+    public void setPartialDynamicObjects(List<WorldShape> partialDynamicObjects) {
+        this.partialDynamicObjects = partialDynamicObjects;
+    }
+
     private static class ElementMgrHolder {
+
         private static final DirectorMgr INSTANCE = new DirectorMgr();
     }
     //</editor-fold>
+    
+    public void handleGameLoop(Long time){
+        partialDynamicObjects.stream().forEach((worldShape) -> {
+            worldShape.redraw();
+        });
+    }
 
-    //public List<WorldShape>[][][] = new ArrayList<>[][][];
+    public void addPartialObject(WorldShape worldShape){
+        partialDynamicObjects.add(worldShape);
+    }
     
 }
